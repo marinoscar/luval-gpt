1e55ebe62ee12446d42fc1a37fd540270a0efd32
Oscar Marin <oscar@marin.cr>
2023-11-26 11:18:31 -0600

    improvements

---
+++ code/Luval.GPT/Logging/AppLogger.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Logging;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luval.GPT.Logging
+{
+    public class AppLogger : Microsoft.Extensions.Logging.ILogger
+    {
+
+        private readonly Logger _logger;
+        public AppLogger(Logger logger)
+        {
+            _logger = logger;
+        }
+
+        public IDisposable? BeginScope<TState>(TState state) where TState : notnull
+        {
+            return null;
+        }
+
+        public bool IsEnabled(Microsoft.Extensions.Logging.LogLevel logLevel)
+        {
+            return _logger.IsEnabled(Map(logLevel));
+        }
+
+        public void Log<TState>(Microsoft.Extensions.Logging.LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+        {
+            if (!IsEnabled(logLevel)) return;
+            _logger.Log(Map(logLevel), exception, formatter(state, exception));
+        }
+
+        private NLog.LogLevel Map(Microsoft.Extensions.Logging.LogLevel level)
+        {
+            switch (level)
+            {
+                case Microsoft.Extensions.Logging.LogLevel.Debug: return NLog.LogLevel.Debug;
+                case Microsoft.Extensions.Logging.LogLevel.Warning: return NLog.LogLevel.Warn;
+                case Microsoft.Extensions.Logging.LogLevel.Information: return NLog.LogLevel.Info;
+                case Microsoft.Extensions.Logging.LogLevel.Trace: return NLog.LogLevel.Trace;
+                case Microsoft.Extensions.Logging.LogLevel.Error: return NLog.LogLevel.Error;
+                case Microsoft.Extensions.Logging.LogLevel.Critical: return NLog.LogLevel.Fatal;
+                default: return NLog.LogLevel.Info;
+            }
+        }
+    }
+}
---
+++ code/Luval.GPT/Logging/LoggingExtensions.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luval.GPT.Logging
+{
+    public static class LoggingExtensions
+    {
+        public static IServiceCollection AddLogging(this IServiceCollection services, Logger logger)
+        {
+            services.AddSingleton<Microsoft.Extensions.Logging.ILogger>(new AppLogger(logger));
+            return services;
+        }
+
+        public static IServiceCollection AddLogging(this IServiceCollection services, LoggingConfiguration config)
+        {
+            // Apply config           
+            NLog.LogManager.Configuration = config;
+            return AddLogging(services, NLog.LogManager.GetCurrentClassLogger());
+        }
+
+        public static IServiceCollection AddLogging(this IServiceCollection services)
+        {
+            var config = new NLog.Config.LoggingConfiguration();
+
+            // Targets where to log to: File and Console
+            var logfile = new NLog.Targets.FileTarget("logfile") { FileName = "file.txt" };
+            var logconsole = new NLog.Targets.ConsoleTarget("logconsole");
+
+            // Rules for mapping loggers to targets            
+            config.AddRule(NLog.LogLevel.Debug, NLog.LogLevel.Fatal, logconsole);
+            config.AddRule(NLog.LogLevel.Debug, NLog.LogLevel.Fatal, logfile);
+
+            //Send config
+            return AddLogging(services, config);
+
+        }
+    }
+}
