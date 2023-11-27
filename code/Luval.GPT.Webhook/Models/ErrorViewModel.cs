df1c561cfdc035c9916c3929371e8c98f957c77e
Oscar Marin <oscar@marin.cr>
2023-11-26 20:32:22 -0600

    fixes and remove

--- code/Luval.GPT.Webhook/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Luval.GPT.Webhook.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- code/Luval.GPT/Logging/LoggingExtensions.cs
+++ code/Luval.GPT/Logging/LoggingExtensions.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Amazon.Runtime;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using NLog;
+using NLog.AWS.Logger;
 using NLog.Config;
+using NLog.Targets;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,21 +28,37 @@ namespace Luval.GPT.Logging
             return AddLogging(services, NLog.LogManager.GetCurrentClassLogger());
         }
 
-        public static IServiceCollection AddLogging(this IServiceCollection services)
+        public static IServiceCollection AddLogging(this IServiceCollection services, BasicAWSCredentials credentials)
         {
             var config = new NLog.Config.LoggingConfiguration();
 
             // Targets where to log to: File and Console
-            var logfile = new NLog.Targets.FileTarget("logfile") { FileName = "file.txt" };
+            //var logfile = new NLog.Targets.FileTarget("logfile") { FileName = "file.txt" };
             var logconsole = new NLog.Targets.ConsoleTarget("logconsole");
+            var aws = GetAWSTarget(config, credentials);
 
             // Rules for mapping loggers to targets            
             config.AddRule(NLog.LogLevel.Debug, NLog.LogLevel.Fatal, logconsole);
-            config.AddRule(NLog.LogLevel.Debug, NLog.LogLevel.Fatal, logfile);
+            //config.AddRule(NLog.LogLevel.Debug, NLog.LogLevel.Fatal, logfile);
 
             //Send config
             return AddLogging(services, config);
 
         }
+
+        private static AWSTarget GetAWSTarget(LoggingConfiguration config, BasicAWSCredentials credentials)
+        {
+            var awsTarget = new AWSTarget()
+            {
+                LogGroup = "LuvalGPT.Logs",
+                Region = "us-east-1"
+            };
+            awsTarget.Credentials = credentials;
+            //config.AddTarget("aws", awsTarget);
+            config.AddRule(NLog.LogLevel.Debug, NLog.LogLevel.Fatal, awsTarget);
+            return awsTarget;
+        }
+
+
     }
 }
