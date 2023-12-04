52be0e9b0b994d5a7ca3ada754c5d5c7a8d012f0
Oscar Marin <oscar@marin.cr>
2023-12-03 20:00:25 -0600

    improvements

--- code/Luval.WebPush/wwwroot/ServiceWorker.js
+++ code/Luval.WebPush/wwwroot/ServiceWorker.js
@@ -1,43 +1,21 @@
 ﻿self.addEventListener('fetch', function (event) { });
 
 self.addEventListener('push', function (e) {
-    var body;
-    var payloadOptions;
+    var options;
     if (e.data) {
-        payloadOptions = JSON.parse(e.data.text())
-        body = payloadOptions.body;
+        options = JSON.parse(e.data.text())
     } else {
-        body = "Standard Message";
+        throw new Error('Invalid notification object');
     }
     
-
-    var options = {
-        body: body,
-        icon: "images/icon-512x512.png",
-        vibrate: [100, 50, 100],
-        data: {
-            dateOfArrival: Date.now()
-        },
-        actions: [
-            {
-                action: "explore", title: "Go interact with this!",
-                icon: "images/checkmark.png"
-            },
-            {
-                action: "close", title: "Ignore",
-                icon: "images/red_x.png"
-            },
-        ]
-    };
     e.waitUntil(
-        self.registration.showNotification("Push Notification", payloadOptions)
+        self.registration.showNotification("Push Notification", options)
     );
 });
 
 self.addEventListener('notificationclick', function (e) {
     var notification = e.notification;
     var action = e.action;
-    console.log(e);
     if (action === 'close') {
         notification.close();
     } else {
